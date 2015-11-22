678306087196ff281cf6c566af7a56eb112a8167
Jon Hall <jon_hall@outlook.com>
2015-11-22 15:12:44 +0000

    improve error handling in service to log a stack trace and detailed error info (when possible)

--- src/service.js
+++ src/service.js
@@ -1,12 +1,11 @@
 'use strict';
 
 const pm2 = require('pm2'),
-    assert = require('assert'),
     start_script = process.env.PM2_SERVICE_SCRIPT || process.env.PM2_SERVICE_CONFIG;
 
 // NOTE: 'true' means the PM2 daemon exists in this process, so it gets kept alive with us as a Windows service
 pm2.connect(true, function(err) {
-    assert.ifError(err);
+    handleError(err);
 
     if(!start_script) {
         // No start script so just try and ressurect
@@ -16,7 +15,18 @@ pm2.connect(true, function(err) {
     } else {
         // Else, try to start the start script (js file or json config)
         pm2.start(start_script, function(err2) {
-            assert.ifError(err2);
+            handleError(err2);
         });
     }
 });
+
+function handleError(err) {
+    if(err) {
+        if(err instanceof Error) {
+            throw err;
+        }
+
+        // We stringify since PM2 chucks us back objects that just end up as [Object object] otherwise
+        throw new Error(JSON.stringify(err));
+    }
+}
